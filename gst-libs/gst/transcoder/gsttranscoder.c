77594bb110ff9cf486a4e6c8e56fa62826b88db0
Marc Leeman <marc.leeman@gmail.com>
2025-05-30 09:28:27 +0200

    New upstream version 1.26.2

--- gst-libs/gst/transcoder/gsttranscoder.c
+++ gst-libs/gst/transcoder/gsttranscoder.c
@@ -263,6 +263,7 @@ gst_transcoder_finalize (GObject * object)
   g_free (self->dest_uri);
   g_cond_clear (&self->cond);
   gst_object_unref (self->api_bus);
+  gst_clear_object (&self->profile);
 
   G_OBJECT_CLASS (parent_class)->finalize (object);
 }
@@ -323,6 +324,7 @@ gst_transcoder_set_property (GObject * object, guint prop_id,
       gst_transcoder_set_position_update_interval_internal (self);
       break;
     case PROP_PROFILE:
+      /* G_PARAM_CONSTRUCT_ONLY */
       GST_OBJECT_LOCK (self);
       self->profile = g_value_dup_object (value);
       GST_OBJECT_UNLOCK (self);
